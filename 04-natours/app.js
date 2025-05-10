fd3f68377d307acb82ebfcca9a85d6bbb65d9d9b
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-05-10 15:44:55 +0700

    express

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -1,31 +1,11 @@
-const express = require('express');
-const morgan = require('morgan');
+"use strict";
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const express = require("express");
 
 const app = express();
 
-// 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
-
-// 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-
-module.exports = app;
